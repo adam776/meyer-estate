fff413ecd90d664b47480c622afeea28a1b656d1
MAC <mac@MACs-MacBook-Pro.local>
2024-11-10 15:46:00 -0500

    fixed authentication problem

--- api/index.js
+++ api/index.js
@@ -1,21 +1,13 @@
-import express from 'express';
-import mongoose from 'mongoose';
+import express from "express";
+import mongoose from "mongoose";
 import dotenv from 'dotenv';
 
-// Load environment variables from .env file
 dotenv.config();
 
-// MongoDB connection URI
-const mongoURI = process.env.MONGO_URI;  // Make sure to define this in your .env file
-
-// Connect to MongoDB
-mongoose.connect(mongoURI, { useNewUrlParser: true, useUnifiedTopology: true })
-  .then(() => console.log('MongoDB connected successfully'))
-  .catch((err) => console.error('MongoDB connection error:', err));
+mongoose.connect(process.env.MONGO)
 
 const app = express();
 
-// Start the server
-app.listen(3000, () => {
-    console.log('Server is running on port 3000!!!');
-});
+app.listen(3000, () =>{
+  console.log("Server is running on port 3000!")
+})
